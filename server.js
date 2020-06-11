f17204e641774219e6e842a0d82ab2e8969bed54
Melanie <melanie-ponce@hotmail.com>
2020-06-11 18:23:50 -0400

    add get and post method - missing delete method

---
+++ server.js
@@ -0,0 +1,96 @@
+//Dependencies
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+//Express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Express for data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//Routes
+app.get("/", function (req, res) {
+  console.log("route check");
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+//API Routes
+app.get("/api/notes", function (request, response) {
+  response.json(db);
+});
+
+app.post("/api/notes", function (request, response) {
+  db.push(request.body);
+  response.json(db);
+});
+
+app.delete("/api/notes/:id", function (request, response) {
+
+    db.forEach((item, i) => {
+        request.params.id = i + 1;
+        console.log('Note ID: ', request.params.id)
+    });
+
+    // var id = request.params.id;
+
+    // let articles = await Article.findAll({tag: id}).exec();
+
+    // response.render('notes', {
+    //     articles: articles
+    // });
+//     var identifier = req.params(id);
+
+//   console.log(identifier);
+
+//   for (var i = 0; i < identifier.length; i++) {
+//     if (identifier === id[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+
+
+//   var id = request.param("id");
+  
+//   MyModel.remove({
+//       _id: id 
+//   }, function(err){
+//       if (err) {
+//           console.log(err)
+//       }
+//       else {
+//          return res.send("Removed");
+//       }
+//   });
+//   console.log(appID);
+
+  //   for (var i = 0; i < characters.length; i++) {
+  //     if (chosen === characters[i].routeName) {
+  //       return res.json(characters[i]);
+  //     }
+  //   }
+
+  //   return res.json(false);
+});
+
+// ROUTES
+// API
+// '/api/notes'
+// DELETE delete a note
+// receive ID parameter
+// compare it with the IDs in db.json
+// match the ID and delete that note
+
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
